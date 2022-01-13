38a94f70b57fbe67f1cc76de63faac5b66b9cd5d
Lucas Chociay <lcschy@icloud.com>
2022-01-13 13:52:06 -0300

    test: add test case for submit btn

--- cypress/integration/collect-atomic-banks-with-elements/bank.spec.js
+++ cypress/integration/collect-atomic-banks-with-elements/bank.spec.js
@@ -8,6 +8,35 @@ context('bank tokenization', () => {
     cy.wait(1500);
   });
 
+  it('should enable/disable submit btn based on change events', () => {
+    cy.get('button').contains('Submit').should('be.disabled');
+
+    cy.get('#routing_number>iframe').iframe(() => {
+      cy.get('input').eq(0).type('021000021');
+    });
+
+    cy.get('button').contains('Submit').should('be.disabled');
+
+    cy.get('#account_number>iframe').iframe(() => {
+      cy.get('input')
+        .eq(0)
+        .type(
+          chance.string({
+            numeric: true,
+            length: 10,
+          })
+        );
+    });
+
+    cy.get('button').contains('Submit').should('be.enabled');
+
+    cy.get('#routing_number>iframe').iframe(() => {
+      cy.get('input').eq(0).type('{backspace}');
+    });
+
+    cy.get('button').contains('Submit').should('be.disabled');
+  });
+
   it(`should tokenize/fund bank`, () => {
     cy.intercept('POST', 'https://api.basistheory.com/atomic/banks').as(
       'tokenize'
