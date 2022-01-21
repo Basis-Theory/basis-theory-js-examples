1b019b3f85a41a974d96742a4f090c955da04827
Davi Aquino <djejaquino@gmail.com>
2022-01-21 18:07:39 -0300

    feat: wip card collecting example

---
+++ collect-atomic-cards-with-elements-react/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  useBasisTheory,
+  BasisTheoryProvider,
+} from '@basis-theory/basis-theory-react';
+import './App.css';
+import { CheckoutForm } from './CheckoutForm';
+
+function App() {
+  const { bt } = useBasisTheory('key_XVB48UzHJ57TdPtmLhJa9e', {
+    elements: true,
+  });
+
+  return (
+    <BasisTheoryProvider bt={bt}>
+      <div className="container">
+        <CheckoutForm />
+      </div>
+    </BasisTheoryProvider>
+  );
+}
+
+export default App;
